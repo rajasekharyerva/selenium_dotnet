69a7c1ea59fbb1a73c788bd6c89fac5d8864d2e8
Y Rajasekhar Reddy <sekharan7@gmail.com>
2024-11-17 08:14:26 +0530

    first commit:

---
+++ tests/FirstTest.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace selenium_dotnet.tests
+{
+    // Marking this class as a test fixture in NUnit
+    [TestFixture]
+    public class FirstTest
+    {
+        // Declare WebDriver for the test
+        private IWebDriver driver;
+
+        // This method runs before each test method (initial setup)
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize ChromeDriver (make sure chromedriver is in your PATH or specify its location)
+            driver = new ChromeDriver();
+        }
+
+        // This is a test method marked with NUnit's [Test] attribute
+        [Test]
+        public void OpenGoogleAndCheckTitle()
+        {
+            // Navigate to a URL
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            // Get the page title
+            string title = driver.Title;
+
+            // Assert that the title contains the word "Google"
+             Assert.That(title.Contains("Google"), "Title does not contain 'Google'");
+        }
+
+        // This method runs after each test method (cleanup)
+        [TearDown]
+        public void TearDown()
+        {
+            // Close the browser and quit the driver
+            driver.Quit();
+        }
+    }
+}
